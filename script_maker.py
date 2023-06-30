3f397cbf2fec1a0791a254f3831ff6defcb17ffe
nolonunez <manueln2207@hotmail.com>
2023-06-30 02:46:08 -0600

    new images2

--- script_maker.py
+++ script_maker.py
@@ -2,7 +2,7 @@ from json_maker import script_make
 
 name = "Trouble Brewing Spanish"
 author = "Nolo"
-logo = ""
+logo = "https://www.bloodstar.xyz/p/nolo/Trouble_Brewing_ES_FULL/_meta.png"
 
 roles = [
 "chef",
