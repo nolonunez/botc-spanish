d8cee6585d0c78bbd97de5e6be9cc30aa8dd7e45
nolonunez <manueln2207@hotmail.com>
2023-07-01 02:56:43 -0600

    all roles in amy

--- amy.py
+++ amy.py
@@ -3,32 +3,69 @@ amy = [
 
 #You start knowing
 "grandmother", #Abuelita
+"balloonist", #Aeronauta
+"steward", #Ayudante
 "librarian", #Bibliotecario
+"knight", #Caballero
+"bountyhunter", #Cazarrecompensas
 "chef", #Chef
 "investigator", #Detective
+"pixie", #Hada
 "washerwoman", #Lavandera
+"noble", #Noble
+"clockmaker", #Relojero
 #Each night
+"highpriestess", #Gran Sacerdotisa 
 "chambermaid", #Camarera
+"cultleader", #Cultista
 "empath", #Émpata
+"snakecharmer", #Encanta-Serpientes
+"general", #General
 "sailor", #Marinero
+"mathematician", #Matemático
+"preacher", #Pastor
+"king", #Rey
+"dreamer", #Soñador
 "fortuneteller", #Vidente
 #Each night*
 "exorcist", #Exorcista
+"lycanthrope", #Licántropo
 "gambler", #Ludópata
 "monk", #Monje
+"flowergirl", #Niña de las Flores
+"oracle", #Oráculo
 "undertaker", #Sepulturero
 "inkeeper", #Tabernero
+"towncrier", #Voceador
 #Each day
+"amnesiac", #Amnésico 
 "gossip", #Chismoso
+"savant", #Prodigio
 #Once per game
+"artist", #Artista
+"atheist", #Ateo
+"cannibal", #Caníbal
 "slayer", #Cazador
 "courtier", #Cortesano
+"seamstress", #Costurera
+"poppygrower", #Jardinera
+"philosopher", #Filósofo
+"huntsman", #Guardabosques
+"engineer", #Ingeniero
+"fisherman", #Pescador
 "professor", #Profesor
+"nightwatchman", #Vigilante Nocturno
 #Everything else
 "mayor", #Alcalde
+"alchemist", #Alquimista
 "fool", #Bufón
+"choirboy", #Corista
 "ravenkeeper", #Cuida-cuervos
+"farmer", #Granjero
+"magician", #Mago
+"juggler", #Malavarista
 "pacifist", #Pacifista
+"sage", #Sabio
 "tealady", #Señora del Té
 "soldier", #Soldado
 "minstrel", #Trovador
@@ -39,59 +76,122 @@ amy = [
 #Each night
 "butler", #Mayordomo
 #Each night*
+"acrobat", #Acróbata
 #Each day
 #Once per game
 #Everything else
+"sweetheart", #Amor de Persona
+"barber", #Barbero
 "drunk", #Borracho
+"damsel", #Damisela
+"golem", #Gólem
+"heretic", #Herético
 "moonchild", #Hijo de la Luna
+"puzzlemaster", #Maestro del Acertijo
 "goon", #Matón
+"mutant", #Mutant
 "lunatic", #Lunático
+"politician", #Político
 "recluse", #Reclusa
 "saint", #Santo
+"snitch", #Soplón
+"klutz", #Torpe
 "tinker", #Trabajador
 
 #MINIONS
 #You start knowing
+"mezepheles", #Mezefeles
 "godfather", #Padrino
+"widow", #Viuda
 #Each night
-"devilsadvocate", #Abogado del Diablo
+"devilsadvocate", #Abogado Corrupto
+"witch", #Bruja
+"cerenovus", #Cerenovus
 "poisoner", #Envenenador
 "spy", #Espía
+"fearmonger", #Pesadilla
 #Each night*
+"pithag", #Pit-Hag
 #Each day
+"psychopath", #Psicópata
 #Once per game
 "assassin", #Asesino
 #Everything else
 "baron", #Barón
+"goblin", #Duende
+"eviltwin", #Gemela Malvada
 "mastermind", #Maquiavélico
+"marionette", #Marioneta
 "scarletwoman", #Mujer Escarlata
+"boomdandy", #Señor Bombas
+"organgrinder", #Organillero
+"vizier", #Visir
 
 #DEMONS
+#You start...
+"lleech", #Sanguijuela 
 #Each night
+"lilmonsta", #Mounstrín
 "pukka", #Pukka
 #Each night*
+"alhadikhia", #Al-Hadikia
+"fanggu", #Fang Gu
 "imp", #Imp
+"legion", #Legión
+"nodashii", #No Dashii
 "po", #Po
 "shabaloth", #Shabaloth
+"vigormortis", #Vigormortis
+"vortox", #vortox
 "zombuul", #Zombuul
+#Everything else
+"riot", #Furia
+"leviathan", #Leviatán
+
 
 
 #TRAVELERS
 #You start knowing
 #Each night
+"barista", #Barista
 "bureaucrat", #Burócrata
 "thief", #Ladrón
 #Each night*
+"harlot", #Mujerzuela
 #Each day
+"butcher", #Carnicero
+"gangster", #Gangster
 "matron", #Matriarca
 "gunslinger", #Pistolero
 #Once per game
+"bonecollector", #Huesero
 "judge", #Juez
 #Everything else
 "apprentice", #Aprendíz
 "scapegoat", #Chivo Expiatorio
 "beggar", #Limosnero
+"deviant", #Degenerado
 "bishop", #Obispo
 "voudon", #Vudú
+
+#FABLED
+#Any game
+"doomsayer",
+"angel",
+"buddhist",
+"hellslibrarian",
+"revolutionary",
+"fiddler",
+"toymaker",
+#Custom games
+"fibbin",
+"duchess",
+"sentinel",
+"spiritofivory",
+"djinn",
+#Experimental
+"stormcatcher",
+"bootlegger",
+"gardener"
 
 ]
--- base_scripts.py
+++ base_scripts.py
@@ -2,7 +2,7 @@
 
 #TROUBLE BREWING
 
-name = "Trouble Brewing"
+name = "Destilando Problemas"
 author = ""
 logo = "https://www.bloodstar.xyz/p/nolo/Trouble_Brewing_ES_FULL/_meta.png"
 
@@ -34,4 +34,43 @@ roles = [
 "bureaucrat",
 "gunslinger",
 "scapegoat"
+]
+
+#BAD MOON RISING
+
+name = "Luna de Sangre"
+author = ""
+logo = ""
+
+roles = [
+"grandmother",
+"sailor",
+"chambermaid",
+"exorcist",
+"innkeeper",
+"gambler",
+"gossip",
+"courtier",
+"professor",
+"minstrel",
+"tealady",
+"pacifist",
+"fool",
+"goon",
+"lunatic",
+"tinker",
+"moonchild",
+"godfather",
+"devilsadvocate",
+"assassin",
+"mastermind",
+"zombuul",
+"pukka",
+"shabaloth",
+"po",
+"apprentice",
+"matron",
+"voudon",
+"judge",
+"bishop"
 ]
