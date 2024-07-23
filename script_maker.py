6bf8f0b4727c255cc76958c0fb1159d4a8aa32f1
Manuel Nuñez <manueln2207@hotmail.com>
2024-07-23 15:15:22 -0600

    sweetheart outsider

--- script_maker.py
+++ script_maker.py
@@ -1,10 +1,34 @@
 from json_maker import script_make
 from base_scripts import tb,snv,bmr
 
-name = "Luna de Sangre"
+name = "Muerte Incierta"
 author = ""
 logo = ""
 
-roles = bmr
+roles = [
+    "clockmaker",
+    "grandmother",
+    "librarian",
+    "empath",
+    "fortuneteller",
+    "exorcist",
+    "flowergirl",
+    "oracle",
+    "undertaker",
+    "monk",
+    "artist",
+    "slayer",
+    "seamstress",
+    "lunatic",
+    "mutant",
+    "sweetheart",
+    "recluse",
+    "godfather",
+    "assassin",
+    "scarletwoman",
+    "marionette",
+    "nodashii",
+    "pukka"
+]
 
 script_make(name,author,logo,roles)
