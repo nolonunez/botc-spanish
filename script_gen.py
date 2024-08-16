606c3d96d5d0acb7bdc02f72ca2710fcc62d8cf4
Manuel Nuñez <manueln2207@hotmail.com>
2024-08-15 18:29:38 -0600

    base 3 pdfs

--- script_gen.py
+++ script_gen.py
@@ -1,39 +1,13 @@
 from assets.json_app import script
 from assets.base_scripts import tb,snv,bmr
 
-name = "El Proyecto Chaneque"
-author = "Nolo"
+name = "Luna de Sangre"
+author = "Nolo (trad.)"
 logo = ""
 background = ""
 #'Y' or 'N'
-pdf = "N"
+pdf = "Y"
 
-roles = [
-    'noble',
-    'grandmother',
-    'pixie',
-    'empath',
-    'dreamer',
-    'balloonist',
-    'gambler',
-    'monk',
-    'nightwatchman',
-    'philosopher',
-    'farmer',
-    'cannibal',
-    'banshee',
-    'ogre',
-    'sweetheart',
-    'mutant',
-    'drunk',
-    'widow',
-    'harpy',
-    'summoner',
-    'assassin',
-    'yaggababble',
-    'imp',
-    'po',
-    'vigormortis'
-]
+roles = bmr
 
 script(name,author,logo,background,roles,pdf)
